6f31f1e4f7de54a236311850e9e6aee114af6898
Kishoreshan-01 <115439796+Kishoreshan-01@users.noreply.github.com>
2022-10-13 19:24:42 +0530

    Time: 954 ms (35.01%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,25 +1,7 @@
 /* Write your T-SQL query statement below */
 
-/*
-with cte
-as
-(
-    Select email, row_number() over(partition by email order by email) as rn
-    From Person
-)
-Select distinct email as Email
-from cte
-where rn > 1 
-*/
 
-
-with cte
-as
-(
-    Select email, count(1) as cnt
-    From Person
-    Group by email
-)
-Select email as Email
-from cte
-where cnt > 1 
+Select Email
+from person
+group By Email
+having Count(Email) > 1
