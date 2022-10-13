7c7e43b557ce410615ddb9410533afc6a82f36e1
Kishoreshan-01 <115439796+Kishoreshan-01@users.noreply.github.com>
2022-10-13 19:12:27 +0530

    Time: 961 ms (50.94%), Space: 0B (100.00%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,7 +1,6 @@
 /* Write your T-SQL query statement below */
 Select e.name as Employee
 from Employee as e
-left join Employee as m
+Join Employee as m
 on e.managerId = m.id
-where m.Id is not null
 and e.salary > m.salary
